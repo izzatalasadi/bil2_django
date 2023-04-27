2ca1400b2f078e4189ff017700937f72efbf3350
izzatfahmialasadi <izzat.alasadi@gmail.com>
2023-04-27 09:33:27 +0200

    bil2-website

--- apps/home/apps.py
+++ apps/home/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 from django.db.models import BigAutoField
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-from .content_scraper import FinnCarSpider
-
 
 class MyAppConfig(AppConfig):
     name = 'apps.home'
