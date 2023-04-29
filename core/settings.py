69fc8db7e48770259b0fc27d95e8e0331b9666e0
izzatfahmialasadi <izzat.alasadi@gmail.com>
2023-04-29 18:26:44 +0200

    bil2

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', 'localhost:85', '127.0.0.1', '0.0.0.0',  # '192.168.1.9',
+ALLOWED_HOSTS = ['localhost', 'localhost:85', '127.0.0.1', '0.0.0.0',  'bil2.herokuapp.com',
                  env('SERVER', default='127.0.0.1')]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1'
                         'https://' + env('SERVER', default='127.0.0.1')]
